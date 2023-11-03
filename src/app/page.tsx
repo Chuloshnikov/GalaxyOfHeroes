8d3ac4d845da35a6b6c320e3f422d165366ea5f3
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-03 14:53:49 +0200

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,11 @@
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Home page',
-  description: '',
+  title: 'Galaxy of Heroes',
+  description: 'Galaxy of Heroes: Your Ultimate Comic Book Haven',
+  icons: {
+    icon: '/logo.svg',
+  }
 }
 
 export default function Home() {
