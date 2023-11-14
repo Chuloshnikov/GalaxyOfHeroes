6affb88c47b2f01c908f7c3f2a7ef935f2b9b3d4
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-14 16:54:00 +0200

    done

---
+++ src/app/[lang]/dashboard/page.tsx
@@ -0,0 +1,30 @@
+import { Locale } from '../../../../i18n.config';
+import { getDictionary } from '../../../lib/dictionary';
+
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../api/auth/[...nextauth]/route';
+
+
+export default async function Dashboard({
+    params: [ lang ]
+}: {
+    params: { lang: Locale }
+}) {
+    const { page, auth } = await getDictionary(lang);
+    const session = await getServerSession(authOptions);
+    const user = session?.user
+
+    return (
+        <section>
+            <div>
+                <h1>{page.dashboard.title}</h1>
+                <p>{page.dashboard.description}</p>
+                <div>
+                    <pre>
+                        <code>{JSON.stringify({ name: user?.name}, null, 2)}</code>
+                    </pre>
+                </div>
+            </div>
+        </section>
+    )
+}
