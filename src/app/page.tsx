e30d52601eea9ed3704fda9bf609ae41b2730eb3
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-11-16 17:05:57 +0200

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,26 @@
-import Intro from "@/components/Intro";
-import type { Metadata } from 'next';
+"use client"
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation'
 
-export const metadata: Metadata = {
-  title: 'Galaxy of Heroes',
-  description: 'Galaxy of Heroes: Your Ultimate Comic Book Haven',
-  icons: {
-    icon: '/logo.svg',
-  }
-}
+
 
 function HomePage() {
-  return (
-    <>
-    <Intro/>
-    </>
-  )
+  const router = useRouter()
+
+  useEffect(() => {
+    const browserLang = window.navigator.language
+    if (browserLang === "en-US") {
+      router.push(`/en`)
+    } else if (browserLang === "uk-UA") {
+      router.push(`/ua`)
+    } else if (browserLang === "de-DE") {
+      router.push('/de')
+    } else {
+      router.push(`/en`)
+    }
+  }, [])
+    
+  return null;
 }
 
 export default HomePage;
