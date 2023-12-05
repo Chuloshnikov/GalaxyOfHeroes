96599c6d48ffa31fff63224d2bb976fc26f39984
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2023-12-05 17:29:57 +0200

    done

---
+++ data/newsData.js
@@ -0,0 +1,41 @@
+export default function getNews() {
+    const news = [
+        {   
+            _id: "1233434",
+            img: "./blacksad.png",
+            title: "New Job - We're hiring a content creator / social media executive",
+            date: "10/12/23",
+            time: "2 min",
+            topic: "ivents",
+            description: "Lorem ipsum sit amet sggsfg fdfgdfg saff fdgdfgd fdfgdfgdgdfgdfgdfg dfgdfgdfgdfg dfgdfgdgdfg fdgdfgdfgdgdfgdfgdf dffgdfgdfgdfgdfgdfg dffgdfgdfgdfg",
+        },
+        {   
+            _id: "12333434",
+            img: "./blacksad.png",
+            title: "New Job - We're hiring a content creator / social media executive",
+            date: "10/12/23",
+            time: "2 min",
+            topic: "ivents",
+            description: "Lorem ipsum sit amet sggsfg fdfgdfg saff fdgdfgd fdfgdfgdgdfgdfgdfg dfgdfgdfgdfg dfgdfgdgdfg fdgdfgdfgdgdfgdfgdf dffgdfgdfgdfgdfgdfg dffgdfgdfgdfg",
+        },
+        {   
+            _id: "12334343344",
+            img: "./blacksad.png",
+            title: "New Job - We're hiring a content creator / social media executive",
+            date: "10/12/23",
+            time: "2 min",
+            topic: "ivents",
+            description: "Lorem ipsum sit amet sggsfg fdfgdfg saff fdgdfgd fdfgdfgdgdfgdfgdfg dfgdfgdfgdfg dfgdfgdgdfg fdgdfgdfgdgdfgdfgdf dffgdfgdfgdfgdfgdfg dffgdfgdfgdfg",
+        }, 
+        {   
+            _id: "1233434343",
+            img: "./blacksad.png",
+            title: "New Job - We're hiring a content creator / social media executive",
+            date: "10/12/23",
+            time: "2 min",
+            topic: "ivents",
+            description: "Lorem ipsum sit amet sggsfg fdfgdfg saff fdgdfgd fdfgdfgdgdfgdfgdfg dfgdfgdfgdfg dfgdfgdgdfg fdgdfgdfgdgdfgdfgdf dffgdfgdfgdfgdfgdfg dffgdfgdfgdfg",
+        },
+    ]
+    return news;
+}
