3da353cbc8b9b3b8180a37ebf6f320eba629c775
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-01-28 07:58:19 +0200

    credentials fix done

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,14 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
 let locales = ["en", "de", "ua"]
  
 // Get the preferred locale, similar to the above or using a library
-function getLocale(request) { 
+function getLocale(request: NextRequest) { 
     return "en";
  }
  
-export function middleware(request) {
+export function middleware(request: NextRequest) {
   // Check if there is any supported locale in the pathname
   const { pathname } = request.nextUrl
   const pathnameHasLocale = locales.some(
@@ -19,7 +22,7 @@ export function middleware(request) {
   request.nextUrl.pathname = `/${locale}${pathname}`
   // e.g. incoming request is /products
   // The new URL is now /en-US/products
-  return Response.redirect(request.nextUrl)
+  return NextResponse.redirect(request.nextUrl)
 }
  
 export const config = {
