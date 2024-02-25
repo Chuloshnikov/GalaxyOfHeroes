a147300f01eda918708c3b56d06244aca3251031
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-02-25 15:11:44 +0200

    done

--- src/app/[lang]/api/categories/route.ts
+++ src/app/[lang]/api/categories/route.ts
@@ -10,11 +10,25 @@ export async function POST(request: NextRequest) {
 }
 
 
+export async function PUT(request: NextRequest) {
+    mongoose.connect(process.env.MONGO_URL);
+    const {_id, name} = await request.json();
+    await Category.updateOne({_id}, {name});
 
+    return Response.json(true);
+}
 
 export async function GET() {
     mongoose.connect(process.env.MONGO_URL);
-    return Response.json(
+    return NextResponse.json(
     await Category.find()
     );
+  }
+
+  export async function DELETE(request: NextRequest) {
+    mongoose.connect(process.env.MONGO_URL);
+    const url = new URL(req.url);
+    const _id = url.searchParams.get('_id');
+      await Category.deleteOne({_id});
+    return NextResponse.json(true);
   }
