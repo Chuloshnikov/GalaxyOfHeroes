3c4df9fa743bd1820382477b5f40a78e0f17248a
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-03-10 19:59:14 +0200

    done

--- src/app/[lang]/api/items/route.ts
+++ src/app/[lang]/api/items/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import mongoose from "mongoose";
 import Items from '../../../../models/Items';
+import {isAdmin} from "../auth/[...nextauth]/route";
 
 export async function POST(request: NextRequest) {
     mongoose.connect(process.env.MONGODB_URL);
@@ -9,3 +10,27 @@ export async function POST(request: NextRequest) {
 
     return new NextResponse(itemDoc);
 }
+
+export async function PUT(request: NextRequest) {
+    mongoose.connect(process.env.MONGODB_URL);
+    const {_id, ...data} = await request.json();
+    await Items.findByIdAndUpdate(_id, data);
+    return NextResponse.json(true);
+}
+
+export async function GET() {
+    mongoose.connect(process.env.MONGODB_URL);
+    return NextResponse.json(
+        await Items.find()
+    )
+}
+
+export async function DELETE(request: NextRequest) {
+    mongoose.connect(process.env.MONGODB_URL);
+    const url = new URL(request.url);
+    const _id = url.searchParams.get('_id');
+    if (await isAdmin()) {
+      await Items.deleteOne({_id});
+    }
+    return Response.json(true);
+  }
