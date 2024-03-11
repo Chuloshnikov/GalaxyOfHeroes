58310ad943673b2edecf0866dd4ac2b70db32ce3
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-03-11 18:41:43 +0200

    done

--- src/components/news/NewsPage.tsx
+++ src/components/news/NewsPage.tsx
@@ -1,8 +1,36 @@
 "use client"
 import {useState, useEffect} from 'react';
+import { useProfile } from '../profile/UseProfile';
 import Link from 'next/link';
+import Image from 'next/image';
 
 const NewsPage = ({text, lang}: {text:any, lang:any}) => {
+  const [news, setNews] = useState<any>([]);
+  const {loading: profileLoading, data: profileData} = useProfile();
+
+  useEffect(() => {
+    fetch('/api/news').then(res => {
+        res.json().then(news => {
+            setNews(news);
+        })
+    })
+}, []);
+
+
+if (profileLoading) {
+  return 'Loading user info...';
+}
+
+if (!profileData.admin) {
+  return (
+  <div
+  className='mt-8 text-center text-lg text-red-500'
+  >
+      Error! Not an admin
+  </div>
+  )
+}
+
   return (
     <div
     className=''
@@ -22,6 +50,24 @@ const NewsPage = ({text, lang}: {text:any, lang:any}) => {
             href={`/${lang}/profile/news/new`}>
                 {text.newItemButton}
             </Link>
+        </div>
+        <div>
+          <div className='grid xs:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1'>
+                {news?.length > 0 && news.map(item => (
+                    <Link 
+                    key={item._id}
+                    href={`news/edit/${item._id}`}
+                    className='bg-mainBg shadow-md rounded-lg p-4 mx-auto'>
+                        <div className='relative'>
+                            <Image src={item.image} alt="item image" width={300} height={300}/>
+                        </div>
+                        <div className='text-center flex flex-col gap-1'>
+                            <span className='text-xl font-semibold text-accentBg'>{item.title}</span>
+                            <span className='text-sm font-semibold'>{item.language}</span>
+                        </div>
+                    </Link>
+                ))}
+            </div>
         </div>
     </div>
   )
