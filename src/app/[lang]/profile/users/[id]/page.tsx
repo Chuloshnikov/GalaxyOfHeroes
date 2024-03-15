370c4259f3d7fb43391bda20d441771e5c639614
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-03-15 18:37:55 +0200

    done

---
+++ src/app/[lang]/profile/users/[id]/page.tsx
@@ -0,0 +1,33 @@
+import ProfileLayout from '@/components/profile/ProfileLayout';
+import { Locale } from '@/i18n.config';
+import { getDictionary } from '../../../../../lib/dictionaries';
+import EditUserPage from '@/components/users/EditUserPage';
+
+
+export default async function EditUser({params: { lang }}: {params: { lang: Locale }}) {
+    const { profilePage, updateUser } = await getDictionary(lang);
+
+    return (
+        <section
+        className="max-w-container mdl:mx-10 xl:mx-auto my-[12px] lg:my-[56px] px-4"
+        >
+            <div
+            className=''
+            >
+                <h1
+                className='text-[28px] mdl:text-[36px] lg:text-[46px] text-accentBg font-medium ml-4 xl:ml-0'
+                >
+                  {profilePage.title}
+                </h1>
+                <p
+                className='text-[16px] mdl:text-[20px] lg:text-[22px] text-accentBg font-medium ml-4 xl:ml-0'
+                >
+                  {profilePage.description}
+                </p>
+                <ProfileLayout lang={lang} text={profilePage}>
+                      <EditUserPage lang={lang} text={updateUser}/>
+                </ProfileLayout>
+            </div>
+        </section>
+    )
+}
