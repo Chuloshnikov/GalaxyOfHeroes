d9164993cacbb3e0e83142c186ef857abbc85f48
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-04-05 17:25:07 +0300

    done

---
+++ src/components/about-us/AboutUsPage.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import cowboy from "../../assets/cowboy.png";
+import dsknight from "../../assets/dsknight.png";
+import satori from "../../assets/satori.png";
+import samurai from "../../assets/samurai.png";
+
+const AboutUsPage = ({lang, text }: {lang: any, text: any}) => {
+  return (
+    <div className="max-w-full">
+        <div className="flex flex-col-reverse mdl:flex-row gap-10">
+            <div className="flex gap-2 justify-center">
+                <div className="flex flex-col gap-2 mt-8">
+                    <div className="rounded-xl">
+                        <Image className="rounded-xl w-[150px] mdl:w-[200px]" src={dsknight} width={200} height={300} alt="cowboy"/>
+                    </div>
+                    <div className="rounded-xl hidden mdl:block">
+                    <Image className="rounded-xl" src={cowboy} width={200} height={300} alt="cowboy"/>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <div className="rounded-xl">
+                        <Image className="rounded-xl w-[150px] mdl:w-[200px]" src={samurai} width={200} height={300} alt="cowboy"/>
+                    </div>
+                    <div className="rounded-xl">
+                        <Image className="rounded-xl hidden mdl:block" src={satori} width={200} height={300} alt="cowboy"/>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-20 max-w-[650px] flex justify-center text-center mdl:text-left">
+                <h1 className="text-3xl mdl:text-4xl xl:text-5xl  text-accentBg font-medium px-2">{text.title}.</h1>
+            </div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default AboutUsPage;
