cfad4493df468ae7be8903c808994fac0e856930
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-04-18 14:58:11 +0300

    done

--- src/components/news/client-container/NewsItem.tsx
+++ src/components/news/client-container/NewsItem.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import Image from "next/image";
 
 const NewsItem = ({ item }) => {
   return (
-    <div>NewsItem</div>
+    <div className='flex flex-col gap-4 items-center justify-center'>
+        <div>
+            <span>{item.topic}</span>
+            <span>{item.createdAt}</span>
+        </div>
+        <div className="w-full max-h-[400px] overflow-hidden rounded-2xl">
+            <Image className="rounded-2xl" src={item.image} width={800} height={400} alt="newsimage"/>
+        </div>
+        <div className="text-accentBg max-w-[800px] flex flex-col gap-4">
+            <h2 className="text-2xl font-bold">{item.title}</h2>
+            <p className="">{item.description}</p>
+        </div>
+    </div>
   )
 }
 
