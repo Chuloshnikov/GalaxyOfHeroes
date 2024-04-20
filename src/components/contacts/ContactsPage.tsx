8180952bdba829e7cebc357d18b8f8d4f30c54b6
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-04-20 22:00:10 +0300

    done

--- src/components/contacts/ContactsPage.tsx
+++ src/components/contacts/ContactsPage.tsx
@@ -3,7 +3,13 @@ import React from 'react'
 const ContactsPage = ({ lang, text }) => {
   return (
     <div className="max-w-full mb-16">
-        <h2 className="text-3xl mdl:text-4xl xl:text-5xl text-accentBg font-medium px-2 max-w-[520px]">{text.title}</h2>
+        <div className='flex flex-col gap-8'>
+            <h2 className="text-3xl mdl:text-4xl xl:text-5xl text-accentBg font-medium px-2 max-w-[520px]">{text.title}</h2>
+            <p className="text-accentBg font-medium px-2 max-w-[500px]">{text.subTitle}</p>
+        </div>
+        <div className='flex flex-col mdl:flex-row-reverse gap-8'>
+
+        </div>
     </div>
   )
 }
